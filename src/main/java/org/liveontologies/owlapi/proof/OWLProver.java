593d9c082e6fcd80bd3cb47fd99a1397065ad941
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2017-10-13 12:35:57 +0200

    Improve javadoc

--- src/main/java/org/liveontologies/owlapi/proof/OWLProver.java
+++ src/main/java/org/liveontologies/owlapi/proof/OWLProver.java
@@ -24,7 +24,6 @@ package org.liveontologies.owlapi.proof;
 import org.liveontologies.puli.DynamicProof;
 import org.liveontologies.puli.Inference;
 import org.liveontologies.puli.Inferences;
-import org.liveontologies.puli.Proof;
 import org.liveontologies.puli.Proofs;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
@@ -43,15 +42,31 @@ public interface OWLProver extends OWLReasoner {
 
 	/**
 	 * @param entailment
-	 * @return the inferences using which the entailment can be derived from the
-	 *         axioms in the ontology. The inferences deriving the axioms in the
-	 *         ontology can be tested using
-	 *         {@link Inferences#isAsserted(Inference)} and removed from the
-	 *         proof using {@link Proofs#removeAssertedInferences(Proof)}.
+	 *            the {@link OWLAxiom} which needs to be proved
+	 * 
+	 * @return the {@link DynamicProof} using which the given axiom can be
+	 *         derived from the axioms in the ontology, if it is entailed. If
+	 *         the axiom is not entailed, any proof that does not derive the
+	 *         given axiom can be returned, e.g., the empty proof.
+	 * 
+	 *         For deriving axioms that occur in the ontology, the proof should
+	 *         use asserted inferences (for which
+	 *         {@link Inferences#isAsserted(Inference)} returns {@code true}).
+	 * 
+	 *         If the proof for the given axiom changes (i.e., the result of
+	 *         calling this method with the same axiom would be come different,
+	 *         e.g., due to the changes in the ontology), the registered
+	 *         listeners of the {@link DynamicProof} must be notified
+	 *         accordingly.
+	 * 
 	 * @throws UnsupportedEntailmentTypeException
 	 *             if the proof cannot be provided for the given entailment type
+	 *
+	 * @see Proofs#emptyProof()
+	 * @see Inferences#isAsserted(Inference)
+	 * @see DynamicProof.ChangeListener
 	 */
-	public DynamicProof<? extends Inference<OWLAxiom>> getProof(OWLAxiom entailment)
-			throws UnsupportedEntailmentTypeException;
+	public DynamicProof<? extends Inference<OWLAxiom>> getProof(
+			OWLAxiom entailment) throws UnsupportedEntailmentTypeException;
 
 }
